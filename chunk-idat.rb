427049b44376e06f8e4ac1ad6b168c1e7ff4a586
sarahemm <github@sen.cx>
2018-12-05 18:34:57 +0000

    Added more decoding and interpreting of IDAT.
    Now understands data checksums, and added error routine so it validates
    everything as well.

--- chunk-idat.rb
+++ chunk-idat.rb
@@ -10,20 +10,32 @@ class PngChunkIDAT < PngChunk
     info[:IDAT][:compression_level] = compression_level
     info[:IDAT][:preset_dictionary_present] = has_preset_dictionary? ? "Yes" : "No"
     info[:IDAT][:zlib_header_checksum_ok] = zlib_header_checksum_ok? ? "Yes" : "No"
-    # TODO: implement support for preset dictionaries
-    # TODO: check Adler32 data checksum
+    info[:IDAT][:stored_zlib_data_checksum] = stored_zlib_data_checksum
+    info[:IDAT][:actual_zlib_data_checksum] = actual_zlib_data_checksum
     info
   end
 
-  # TODO: implement error checks
+  def errors
+    errors = super
+    errors.push "Invalid compression method" if compression_method == :invalid
+    errors.push "Window size is over 32768" if window_size > 32768
+    errors.push "Has zlib preset dictionary which is disallowed by PNG specification" if has_preset_dictionary?
+    errors.push "Zlib header checksum is incorrect" if !zlib_header_checksum_ok?
+    errors.push "Zlib data checksum is incorrect" if !zlib_data_checksum_ok?
+    errors
+  end
 
-  def uncompressed_size
+  def uncompressed_data
     begin
-      return Zlib::Inflate.inflate(@data).length
+      return Zlib::Inflate.inflate(@data)
     rescue Zlib::DataError
       puts "Error while inflating IDAT data."
-      return -1
+      return ""  # TODO: better handling of corrupt uninflatable files
     end
+  end
+
+  def uncompressed_size
+    uncompressed_data.length
   end
 
   def compression_method
@@ -56,6 +68,18 @@ class PngChunkIDAT < PngChunk
 
   def zlib_header_checksum_ok?
     ((@data[0].ord * 256 + (@data[1].ord & 0x0F)) % 31) == 0
+  end
+  
+  def zlib_data_checksum_ok?
+    actual_zlib_data_checksum == stored_zlib_data_checksum
+  end
+
+  def actual_zlib_data_checksum
+    Zlib::adler32 uncompressed_data
+  end
+
+  def stored_zlib_data_checksum
+    @data[-4..-1].to_s.unpack("N")[0]
   end
 
   def has_preset_dictionary?
